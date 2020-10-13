63a162f6851cf360ca42fb57fb49e2c87ea384d3
Akshay Singla <akshay.singla@chicmic.co.in>
2020-10-13 12:18:20 +0530

    -remove unwanted package

--- app/startup/hm/expressStartup.js
+++ app/startup/hm/expressStartup.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const cors = require('cors');
-const replace = require('replace-in-file');
 
 const routes = require('../../routes');
 const routeUtils = require('../../utils/routeUtils');
